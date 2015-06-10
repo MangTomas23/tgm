81b12c0926240dd4490b06305da81a8fd8477967
MangTomas23 <matos.adrianpaul@gmail.com>
2015-06-10 13:15:03 +0800

    fix migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -32,5 +32,5 @@ class CreateUsersTable extends Migration {
 	{
 		Schema::drop('users');
 	}
-
+//
 }
--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -29,5 +29,5 @@ class CreatePasswordResetsTable extends Migration {
 	{
 		Schema::drop('password_resets');
 	}
-
+//
 }
