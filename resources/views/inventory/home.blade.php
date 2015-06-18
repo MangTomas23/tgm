b5138830b5f074420dba1d1e8a4ec3f7c4b59a53
MangTomas23 <matos.adrianpaul@gmail.com>
2015-06-18 22:50:36 +0800

    fix input type date

--- resources/views/inventory/home.blade.php
+++ resources/views/inventory/home.blade.php
@@ -61,4 +61,14 @@
     </div>
 </div>
 
+<script>
+    $(document).ready( function() {
+        Date.prototype.toDateInputValue = (function() {
+            var local = new Date(this);
+            local.setMinutes(this.getMinutes() - this.getTimezoneOffset());
+            return local.toJSON().slice(0,10);
+        });
+        $('input[type=date]').val(new Date().toDateInputValue());
+    });
+</script>
 @endsection
