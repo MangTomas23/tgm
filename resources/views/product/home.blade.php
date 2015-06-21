4a57ae6ff9661b765726b329aeb0b7ecd30749c6
MangTomas23 <matos.adrianpaul@gmail.com>
2015-06-21 09:13:59 +0800

    table responsive

--- resources/views/product/home.blade.php
+++ resources/views/product/home.blade.php
@@ -25,55 +25,57 @@
     </div>
     {!! Form::close() !!}
     
-    <table id="print-container" class="table table-default" style="margin-top: 20px">
-        <thead>
-            <tr>
-                <th>#</th>
-                <th>Product Name</th>
-                <th>Category</th>
-                <th>Supplier</th>
-                <th>Box</th>
-                <th>Stock</th>
-            </tr>
-        </thead>
-        <tbody>
-            @if($products->isEmpty())
+    <div class="table-responsive">
+        <table id="print-container" class="table table-default" style="margin-top: 20px">
+            <thead>
                 <tr>
-                    <td colspan="5">No records found.</td>
+                    <th>#</th>
+                    <th>Product Name</th>
+                    <th>Category</th>
+                    <th>Supplier</th>
+                    <th>Box</th>
+                    <th>Stock</th>
                 </tr>
-            @else
-                <?php $num = $products->currentPage() * $products->perPage() - $products->perPage(); $n = $num<=0 ? 0:$num?>
-                @foreach($products as $product)
-                    <tr
-                        @if($outOfStock = false) @endif
-                            @foreach($product->boxes as $box)
-                                @if(App\InStock::count($box->id)=='Out of Stock')
-                                    @if($outOfStock = true) @endif
-                                @endif
-                            @endforeach
-                        @if($outOfStock)
-                            class="danger"
-                        @endif
-                    >
-                        <td><?php $n++; echo $n ?></td>
-                        <td><a href="{{ action('ProductController@show', $product->id ) }}">{{ $product->name }}</a></td>
-                        <td>{{ $product->product_category->name or null }}</td>
-                        <td>{{ $product->supplier->name or null }}</td>
-                        <td>
-                            @foreach($product->boxes as $box)
-                                <p class="text-nowrap">{{ $box->size }}</p>
-                            @endforeach
-                        </td>
-                        <td>
-                            @foreach($product->boxes as $box)
-                                <p class="text-nowrap">{{ App\InStock::count($box->id) }}
-                            @endforeach
-                        </td>
+            </thead>
+            <tbody>
+                @if($products->isEmpty())
+                    <tr>
+                        <td colspan="5">No records found.</td>
                     </tr>
-                @endforeach
-            @endif
-        </tbody>
-    </table>
+                @else
+                    <?php $num = $products->currentPage() * $products->perPage() - $products->perPage(); $n = $num<=0 ? 0:$num?>
+                    @foreach($products as $product)
+                        <tr
+                            @if($outOfStock = false) @endif
+                                @foreach($product->boxes as $box)
+                                    @if(App\InStock::count($box->id)=='Out of Stock')
+                                        @if($outOfStock = true) @endif
+                                    @endif
+                                @endforeach
+                            @if($outOfStock)
+                                class="danger"
+                            @endif
+                        >
+                            <td><?php $n++; echo $n ?></td>
+                            <td><a href="{{ action('ProductController@show', $product->id ) }}">{{ $product->name }}</a></td>
+                            <td>{{ $product->product_category->name or null }}</td>
+                            <td>{{ $product->supplier->name or null }}</td>
+                            <td>
+                                @foreach($product->boxes as $box)
+                                    <p class="text-nowrap">{{ $box->size }}</p>
+                                @endforeach
+                            </td>
+                            <td>
+                                @foreach($product->boxes as $box)
+                                    <p class="text-nowrap">{{ App\InStock::count($box->id) }}
+                                @endforeach
+                            </td>
+                        </tr>
+                    @endforeach
+                @endif
+            </tbody>
+        </table>
+    </div>
     
     {!! $products->render() !!}
     
