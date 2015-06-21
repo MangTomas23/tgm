25f6ba25babdfcd4a21ba677199a035ad946976f
MangTomas23 <matos.adrianpaul@gmail.com>
2015-06-22 01:33:56 +0800

    update printable area for suppliers

--- resources/views/supplier/home.blade.php
+++ resources/views/supplier/home.blade.php
@@ -7,50 +7,58 @@
 @section('content')
 
 <div class="container">
-    <div class="page-header">
-        <h1>
-            Suppliers
-            <a href="{{ action('SupplierController@create') }}" class="btn btn-info pull-right">Add</a>
-        </h1>
+    <div class="hidden-print">
+        <div class="page-header">
+            <h1>
+                Suppliers
+                <a href="{{ action('SupplierController@create') }}" class="btn btn-info pull-right">Add</a>
+            </h1>
+        </div>
+
+        <table class="table table-default table-hover">
+            <thead>
+                <tr>
+                    <th>Supplier Name</th>
+                    <th>Contact</th>
+                    <th>Address</th>
+                </tr>
+            </thead>
+            <tbody>
+                @if($suppliers->isEmpty())
+                    <td colspan="3">No Records.</td>
+                @else
+                    @foreach($suppliers as $supplier)
+                        <tr>
+                            <td><a href="{{ action('SupplierController@show', $supplier->id) }}">{{ $supplier->name }}</a></td>
+                            <td>{{ $supplier->contact }}</td>
+                            <td>{{ $supplier->address }}</td>
+                        </tr>
+                    @endforeach
+                @endif
+            </tbody>
+        </table>
+        <hr>
+        <p class="text-right">
+            <strong>No. of Suppliers: </strong> <span class="badge">{{ count($suppliers) }}</span>
+        </p>
     </div>
     
-    <table class="table table-default table-hover">
-        <thead>
-            <tr>
-                <th>Supplier Name</th>
-                <th>Contact</th>
-                <th>Address</th>
-            </tr>
-        </thead>
-        <tbody>
-            @if($suppliers->isEmpty())
-                <td colspan="3">No Records.</td>
-            @else
-                @foreach($suppliers as $supplier)
-                    <tr>
-                        <td><a href="{{ action('SupplierController@show', $supplier->id) }}">{{ $supplier->name }}</a></td>
-                        <td>{{ $supplier->contact }}</td>
-                        <td>{{ $supplier->address }}</td>
-                    </tr>
-                @endforeach
-            @endif
-        </tbody>
-    </table>
+    <div class="col-xs-12 visible-print">
+        <h3><strong>Tradeal General Merchandise</strong><small id="date" class="pull-right"></small></h3>
+        <h4>Suppliers</h4>
+    </div>
     
-    <div class="page-header"></div>
-    <p class="text-right">
-        <strong>No. of Suppliers: </strong> <span class="badge">{{ count($suppliers) }}</span>
-    </p>
     
+    
+    <p class="visible-md-12 hidden-print">Press <kbd>ctrl + p</kbd> to print</p>
+
     <canvas id="myChart"></canvas>
     <canvas id="supplierChart"></canvas>
-    <div class="text-center">
-        <ul id="legend" class="list-inline">
-       
-        </ul>
+    <div class="text-center" style="margin-top: 24px">
+        <ul id="legend" class="list-inline"></ul>
     </div>
-    <style id="legendStyle">
-    </style>
+    
+    <style id="legendStyle"></style>
     
     <script>
         $(document).ready(function(){
@@ -127,11 +135,11 @@
             var suppliers = [];
             
             @foreach($suppliers as $supplier)
-                suppliers.push( '{{ $supplier->name }}' )
+                suppliers.push({name: '{{ $supplier->name }}', productCount: {{ count($supplier->products) }} }  )
             @endforeach
             
             $.each(suppliers, function(i,supplier){
-                $('#legend').append('<li><div class="media-middle legend'+i+'" style="width:12px;height:12px;background-color:'+ colors[0][i] +';display:inline-block;"></div> '+ supplier +'</li>')
+                $('#legend').append('<li><div class="media-middle legend'+i+'" style="width:12px;height:12px;background-color:'+ colors[0][i] +';display:inline-block;"></div> '+ supplier.name +' <span class="badge">'+ supplier.productCount +'</span></li>')
                 $('#legendStyle').append(
                     '@media print{' +
                         '.legend' + i + '{' +
@@ -185,6 +193,17 @@
                         }
             var myPieChart = new Chart(ctx[1]).Pie(data,options);
         });
+        
+        var d = new Date();
+
+        var month = d.getMonth()+1;
+        var day = d.getDate();
+
+        var output = d.getFullYear() + '/' +
+            (month<10 ? '0' : '') + month + '/' +
+            (day<10 ? '0' : '') + day;
+        
+        $('#date').text('Date: ' + output)
         
     </script>
 </div>
