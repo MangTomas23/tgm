1222c80451dd94cffdfbbe64b5480f245155f179
MangTomas23 <matos.adrianpaul@gmail.com>
2015-07-25 12:24:02 +0800

    update migration add product

--- database/migrations/2015_07_25_113150_create_lost_items_table.php
+++ database/migrations/2015_07_25_113150_create_lost_items_table.php
@@ -16,13 +16,16 @@ class CreateLostItemsTable extends Migration {
 		{
 			$table->increments('id');
 			$table->timestamps();
-			$table->integer("lost_id")->unsigned();
-			$table->integer("no_of_box")->unsigned();
-			$table->integer("no_of_packs")->unsigned();
-			$table->decimal("amount", 11, 2)->unsigned();
+			$table->integer('lost_id')->unsigned();
+			$table->integer('no_of_box')->unsigned();
+			$table->integer('no_of_packs')->unsigned();
+			$table->decimal('amount', 11, 2)->unsigned();
+			$table->integer('product_id')->unsigned()->nullable();
 
-			$table->foreign("lost_id")->references("id")->on("losts")
-				->onDelete("cascade");
+			$table->foreign('product_id')->references('id')->on('products')
+				->onDelete('set null');
+			$table->foreign('lost_id')->references('id')->on('losts')
+				->onDelete('cascade');
 		});
 	}
 
