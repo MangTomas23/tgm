33780a0e299c3c78c7419c78c8f3a6629f2a138d
MangTomas23 <matos.adrianpaul@gmail.com>
2015-07-25 12:33:36 +0800

    update LostItem

--- app/LostItem.php
+++ app/LostItem.php
@@ -4,6 +4,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class LostItem extends Model {
 
-	//
+	public function box() {
+		return $this->belongsTo('App\Box');
+	}
+
+	public function product() {
+		return $this->belongsTo('App\Product');
+	}
 
 }
