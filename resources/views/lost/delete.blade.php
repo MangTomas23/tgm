c4fcc118b0259fa47d787b3fdc1ef07e29f158a8
MangTomas23 <matos.adrianpaul@gmail.com>
2015-07-25 19:28:23 +0800

    update delete contents

--- resources/views/lost/delete.blade.php
+++ resources/views/lost/delete.blade.php
@@ -7,6 +7,20 @@
 @section('content')
 
 <div class="container">
+
+	<div class="alert alert-danger">
+		<p>Are you sure you want to delete this record?</p>
+		<div class="text-right">
+			{!! Form::open(['url' => '/lost/item/' . 12, 
+			'method' => 'DELETE']) !!}
+
+			<a href="/lost/item" class="btn btn-default">No</a>
+			{!! Form::submit('Yes', ['class'=>'btn btn-danger']) !!}
+				
+			{!! Form::close() !!}
+			}
+		</div>
+	</div>
 	
 </div>
 
